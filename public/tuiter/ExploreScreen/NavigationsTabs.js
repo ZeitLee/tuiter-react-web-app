958680613b46e8334745965525d5b168a0e66924
ZeitLee <li.zes@husky.neu.edu>
2023-02-19 23:20:21 -0500

    a5 adjust entertainment tab position

--- public/tuiter/ExploreScreen/NavigationsTabs.js
+++ public/tuiter/ExploreScreen/NavigationsTabs.js
@@ -2,33 +2,33 @@ const NavigationsTabs = () => {
     return (`
             <div class="row mb-2">
                 <ul class="nav nav-tabs pt-2">
-                    <li class="nav-item ms-1 px-2">
+                    <li class="nav-item ms-2 px-1">
                         <a class="nav-link active" href="for-you.html">
                             <span class="pb-3">
                                 For you
                             </span>
                         </a>
                     </li>
-                    <li class="nav-item px-2">
+                    <li class="nav-item px-1">
                         <a class="nav-link" href="trending.html">
                             <span class="">
                                 Trending
                             </span>
                         </a>
                     </li>
-                    <li class=" nav-item px-2">
+                    <li class=" nav-item px-1">
                         <a class="nav-link" href="news.html">
                             <span class="">
                                 News
                             </span></a>
                     </li>
-                    <li class=" nav-item px-2">
+                    <li class=" nav-item px-1">
                         <a class="nav-link" href="sports.html">
                             <span class="">
                                 Sports
                             </span></a>
                     </li>
-                    <li class="nav-item px-2">
+                    <li class="nav-item px-1">
                         <a class="nav-link d-none d-md-block" href="entertainment.html">
                             <span class="">
                                 Entertainment
