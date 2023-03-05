7c9dd6038c7fbdfb8af49b9b443a0f4c0ae4761f
ZeitLee <li.zes@husky.neu.edu>
2023-03-05 18:11:22 -0500

    a6 fix pos

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -8,9 +8,8 @@ const ExploreComponent = () => {
             <div className="row">
                 <div className="col-11 position-relative">
                     <input placeholder="Search Tuiter"
-                        className="form-control rounded-pill ps-5" />
-                    <i className="bi bi-search position-absolute 
-                       wd-nudge-up"></i>
+                        className="form-control rounded-pill ps-5 " />
+                    <i className="bi bi-search wd-mag-pos "></i>
                 </div>
                 <div className="col-1">
                     <i className="wd-bottom-4 text-primary float-end bi 
@@ -30,7 +29,7 @@ const ExploreComponent = () => {
             </ul>
             <div className="position-relative mb-2">
                 <img src="/images/starship-main.jpeg" className="w-100" />
-                <h1 className="position-absolute wd-nudge-bottom text-white">
+                <h1 className="position-absolute wd-nudge-up text-white">
                     SpaceX Starship</h1>
             </div>
             <PostSummaryList />
