3ea27887e3533943fcc8b82dcfec19b5686a7841
ZeitLee <li.zes@husky.neu.edu>
2023-03-05 17:26:05 -0500

    a6 add margin bottom and adjust rounded corner

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -11,7 +11,7 @@ function Tuiter() {
     return (
         <>
             <Nav />
-            <div className="row mt-2">
+            <div className="row mt-2 mb-3">
                 <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                     <NavigationSidebar active="explore" />
                 </div>
